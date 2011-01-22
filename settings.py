48f3e0e3a2178d95841c4a3807db9d4e6529e083
Michael Huynh <mike.huynh@gmail.com>
2011-01-22 18:38:45 -0500

    Added my initial settings.py modifications.

--- settings.py
+++ settings.py
@@ -1,4 +1,12 @@
-# Django settings for papers project.
+# Django settings for project.
+import os
+import django
+import sys
+
+# Path of Django framework files (no trailing /):
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+# Path of this "site" (no trailing /):
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -45,17 +53,17 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin/static/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'mmjsh(tm2ky2+bt^-m-+vn-03*o*)fczt2=0zw6mbb#@0_o@wv'
@@ -75,7 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'papers.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -87,10 +95,109 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
+
+    'annoying', #django-annoying
+    'south', #migrations
+    #'django_rpx_plus', 
+    #'celery', #messaging queue
+    'registration', #for email activation
+    #Must come before admin app to override those templates.
+    'registration_defaults', #django-registration default templates
+
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
 )
+
+#The following is not in the default generated settings.py file:
+
+#AUTHENTICATION_BACKENDS = (
+#    #'django_rpx_plus.backends.RpxBackend', 
+#    'django.contrib.auth.backends.ModelBackend', #default django auth
+#)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request', #for request object
+    'django.contrib.messages.context_processors.messages',
+    #Above are the default template context processors
+    #'yourapp.helpers.context_processor',
+)
+
+#Additional user data (ie. User Profile)
+#AUTH_PROFILE_MODULE = 'yourapp.UserProfile'
+
+# Here are some settings related to auth urls. django has default values for them
+# as specified on page: http://docs.djangoproject.com/en/dev/ref/settings/. You
+# can override them if you like.
+#LOGIN_REDIRECT_URL = '' #default: '/accounts/profile/'
+#LOGIN_URL = '' #default: '/accounts/login/'
+#LOGOUT_URL = '' #default: '/accounts/logout/'
+
+#SMTP is the default backend so no need to specify
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST = 'smtp.sendgrid.net'
+#EMAIL_PORT = 587 #TLS
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'appname <do-not-reply@appname.com>'
+#Defines email addresses that error/admin messages come from
+SERVER_EMAIL = 'appname admin <root@appname.com>'
+
+#Keep from getting 404 messages about favicon.ico
+SEND_BROKEN_LINK_EMAILS = False
+
+####################
+# papers settings: #
+####################
+
+
+###################
+# south settings: #
+###################
+
+#We want to disable migrations during tests. Use syncdb instead.
+SOUTH_TESTS_MIGRATE = False
+
+####################
+# celery settings: #
+####################
+
+#CELERY_BACKEND = 'database' #default = database
+
+#BROKER_HOST = 'localhost'
+#BROKER_PORT = 5672
+#BROKER_USER = 'dev'
+#BROKER_PASSWORD = 'testtest'
+#BROKER_VHOST = 'mXs-MBP'
+
+#If True, tasks are executed locally and never sent to queue.
+#CELERY_ALWAYS_EAGER = True
+#CELERYD_LOG_LEVEL = 'INFO'
+
+################################
+#django-registration settings: #
+################################
+
+#Needed if using default backend that requires activation.
+#ACCOUNT_ACTIVATION_DAYS = 30 
+
+#If False, new registration of accounts are closed. (default = True)
+#REGISTRATION_OPEN = False
+
+
+#Import any local settings (ie. production environment) that will override
+#these development environment settings.
+try:
+    from local_settings import *
+except ImportError:
+    pass 
