b6e4ed11ee63f1ee8358052435bea0e521fe8786
Michael Huynh <mike.huynh@gmail.com>
2011-01-23 14:33:53 -0500

    Added dummy home view.

--- papers/views.py
+++ papers/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.conf import settings
+import django.contrib.messages as messages
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+#from django.template import RequestContext
+#from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+#from django.contrib.contenttypes.models import ContentType #for adding perm
+#from django.contrib.auth.models import Permission #for adding perm
+from django.db import models #for aggregator methods
+
+#The reason why we use django's urlencode instead of urllib's urlencode is that
+#django's version can operate on unicode strings.
+#from django.utils.http import urlencode
+
+from annoying.decorators import render_to
+#from annoying.functions import get_object_or_None
+
+#from .forms import 
+#from .models import 
+#from .tasks import 
+
+#import datetime
+
+@render_to('papers/home.html')
+def home(request):
+
+    return {}
