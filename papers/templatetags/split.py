fe177807422d09942323d23df762a8f825c65ea7
Michael Huynh <mike.huynh@gmail.com>
2011-02-01 00:06:42 -0500

    Using splitlines filter instead since split filter is currently broken.

--- papers/templatetags/split.py
+++ papers/templatetags/split.py
@@ -6,4 +6,13 @@ register = template.Library()
 @register.filter
 @stringfilter
 def split(value, delimiter):
+    '''
+    NOTE: This doesn't work right now because django escapes the delimiter!
+    So \n becomes \\n
+    '''
     return value.split(delimiter)
+
+@register.filter
+@stringfilter
+def splitlines(value):
+    return value.splitlines()
