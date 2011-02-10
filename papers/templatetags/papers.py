c160a67d96cd77be2973017a9e26ade21aea6559
Michael Huynh <mike.huynh@gmail.com>
2011-02-10 15:27:06 -0500

    Now takes context to generate the full url.

--- papers/templatetags/papers.py
+++ papers/templatetags/papers.py
@@ -4,12 +4,13 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-@register.simple_tag
-def quickview(paper):
+@register.simple_tag(takes_context = True)
+def quickview(context, paper):
     '''
     Given a Paper object, returns a url to quickview it with Google PDF Preview.
     '''
+    request = context['request']
     quickview_url = 'http://docs.google.com/viewer?embedded=true&url=%s'
-    paper_url = '%s/%s/%s/%s' % (settings.UPLOAD_URL, paper.user.username,
-                              paper.hash, paper.file)
+    paper_url = 'http://%s%s/%s/%s/%s' % (request.get_host(),
+            settings.UPLOAD_URL, paper.user.username, paper.hash, paper.file)
     return quickview_url % paper_url
