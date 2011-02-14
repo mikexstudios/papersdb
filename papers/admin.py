a64df207f738c073f58c3981bd1ad4e3dcbdd71a
Michael Huynh <mike.huynh@gmail.com>
2011-02-13 22:31:32 -0500

    Added 'local_id' to admin

--- papers/admin.py
+++ papers/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Paper, UserProfile
 
 class PaperAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'title', 'authors', 'journal', 'year', 'volume', 
-                    'issue', 'pages', 'url', 'file', 'hash',)
+    list_display = ('id', 'local_id', 'user', 'title', 'authors', 'journal',
+                    'year', 'volume', 'issue', 'pages', 'url', 'file', 'hash',)
     list_display_links = ('id', 'title')
 admin.site.register(Paper, PaperAdmin)
 
