2a00ec5346e07d66160240f757d778570a874bda
Michael Huynh <mike.huynh@gmail.com>
2011-03-24 20:08:10 -0400

    Added passing exists test for individual paper view and edit paper page.

--- papers/tests/exists.py
+++ papers/tests/exists.py
@@ -45,6 +45,7 @@ class LoginRequiredTest(TestCase):
                 '/papers/import/url/9f2b02d1-5d07-4a4c-ae51-48d026a68c6e/',
 
                 '/papers/%s/' % p.local_id, #individual view for paper
+                '/papers/%s/edit/' % p.local_id, 
         )
 
 
@@ -61,8 +62,18 @@ class PageExistsTest(TestCase):
 
     def setUp(self):
         #Create and login test user.
-        u = User.objects.create_user('test', 'test@example.com', 'test')
+        self.user = User.objects.create_user('test', 'test@example.com', 'test')
         self.client.login(username = 'test', password = 'test')
+
+        #Create a sample paper
+        self.data = {'user': self.user, 'title': 'Test Title', 'url':
+                'http://example.com', 'journal': 'Journal of Test', 'year':
+                '2011', 'volume': '1', 'authors': 
+                "Author One\nAuthor Two\nAuthor Three", 'issue': '2', 'pages':
+                '3-4', }
+
+        self.paper = Paper(**self.data) #unpack dictionary to arguments
+        self.paper.save()
 
     def tearDown(self):
         pass
@@ -91,4 +102,23 @@ class PageExistsTest(TestCase):
 
         #Also make sure that a form is being shown. Just check for a Title field
         #in the form. Assume if 'Title' exists, rest of the form does too.
+        self.assertTrue(re.search('Title', str(r.context['form'])))
+
+    def test_papers_new_auto_exists(self):
+        r = self.client.get('/papers/new/', {})
+        self.assertEqual(r.status_code, 200)
+
+        #Also make sure that a form is being shown.
+        self.assertTrue(re.search('URL', str(r.context['form'])))
+
+    def test_papers_view_exists(self):
+        r = self.client.get('/papers/%s/' % self.paper.local_id, {})
+        self.assertEqual(r.status_code, 200)
+
+        self.assertEqual(r.context['paper'].title, self.paper.title) 
+
+    def test_papers_edit_exists(self):
+        r = self.client.get('/papers/%s/edit/' % self.paper.local_id, {})
+        self.assertEqual(r.status_code, 200)
+
         self.assertTrue(re.search('Title', str(r.context['form'])))
