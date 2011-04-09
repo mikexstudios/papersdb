9fd59eb70bacea5bd69ff41dad2b330043309a9f
Michael Huynh <mike.huynh@gmail.com>
2011-04-09 18:22:09 -0400

    Added display of paper thumbnail if exists.

--- papers/templatetags/papers.py
+++ papers/templatetags/papers.py
@@ -8,11 +8,23 @@ register = template.Library()
 @register.simple_tag()
 def paperurl(paper):
     '''
-    Given a Paper object, returns the *relative* url to the paper.
+    Given a Paper object, returns the url (starting with '/') of the paper.
     '''
     paper_url = '%s/%s/%s/%s' % (settings.UPLOAD_URL, paper.user.username,
             paper.hash, paper.file)
     return paper_url
+
+@register.simple_tag()
+def thumbnailurl(paper):
+    '''
+    Given a Paper object, returns the url (starting with '/') to the thumbnail.
+    '''
+    upload_url = '%s/%s/%s' % (settings.UPLOAD_URL, paper.user.username,
+                                  paper.hash)
+    thumbnail_file = settings.THUMBNAIL_FILENAME % paper.hash
+    thumbnail_url = '%s/%s' % (upload_url, thumbnail_file)
+
+    return thumbnail_url
 
 @register.simple_tag(takes_context = True)
 def quickview(context, paper):
