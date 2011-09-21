40000bb2caf820d21d36cd534235c094f2679b2c
Michael Huynh <mike.huynh@gmail.com>
2011-09-20 21:32:33 -0400

    Typo in wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os, sys
 #Adding papersdb directory to path because all of the project paths are 
 #relative to this directory.
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 
-                                'papersdb')
+                                'papersdb'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
