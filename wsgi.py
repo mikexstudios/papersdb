ac710955cdcb8dd713b441515a63cc5513181ed7
Michael Huynh <mike.huynh@gmail.com>
2011-09-20 21:17:49 -0400

    Attempting to fix dotcloud's wsgi path error by including dynamic path in wsgi.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'papersdb.settings'
+import os, sys
+
+#Adding current directory to path because wsgi expects our project to be in a 
+#sub-directory.
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
