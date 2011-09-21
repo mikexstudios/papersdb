8235fa560aa5a8d60327d27a912fa8eae6bf1383
Michael Huynh <mike.huynh@gmail.com>
2011-09-20 21:30:08 -0400

    Fixed dotcloud wsgi path deploy bug.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 import os, sys
 
-#Adding current directory to path because wsgi expects our project to be in a 
-#sub-directory.
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+#Adding papersdb directory to path because all of the project paths are 
+#relative to this directory.
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+                                'papersdb')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
